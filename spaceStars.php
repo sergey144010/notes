d087f4914fbad038d2ebc9f42f205207305e68f9
sergey144010 <sergey144010@mail.ru>
2017-01-08 22:21:09 +0800

    Space and Stars

---
+++ spaceStars.php
@@ -0,0 +1,114 @@
+<?php
+/*
+ * Половина ёлки
+ */
+$n = 10;
+$stars = 1;
+$pr = $n-$stars;
+for($i=1;$i<=$n;$i++){
+
+    for($ii=1; $ii<=$pr; $ii++){
+        print(' ');
+    };
+
+    for($iii=1; $iii<=$stars; $iii++){
+        print('*');
+    };
+
+    print(PHP_EOL);
+
+    $stars++;
+    $pr--;
+};
+
+/*
+ * Ёлка, вариант 1
+ */
+$n = 10;
+$stars = 1;
+$pr = $n-$stars;
+for($i=1;$i<=$n;$i++){
+
+    for($ii=1; $ii<=$pr; $ii++){
+        print(' ');
+    };
+
+    for($iii=1; $iii<=$stars; $iii++){
+        print('*');
+    };
+
+    for($iii=2; $iii<=$stars; $iii++){
+        print('*');
+    };
+
+    print(PHP_EOL);
+
+    $stars++;
+    $pr--;
+};
+
+/*
+ * Ёлка, вариант 2
+ */
+$n = 10;
+$stars = 1;
+$pr = $n-$stars;
+for($i=1;$i<=$n;$i++){
+
+    for($ii=1; $ii<=$pr; $ii++){
+        print(' ');
+    };
+
+    for($iii=1; $iii<=$stars; $iii++){
+        print('*');
+    };
+
+    print(PHP_EOL);
+
+    $stars = $stars+2;
+    $pr--;
+};
+
+/*
+ * Ромб, на основе варианта 2 Ёлка
+ */
+$n = 10;
+$stars = 1;
+$pr = $n-$stars;
+for($i=1; $i<=$n; $i++){
+
+    for($ii=1; $ii<=$pr; $ii++){
+        print(' ');
+    };
+
+    for($iii=1; $iii<=$stars; $iii++){
+        print('*');
+    };
+
+    for($iii=2; $iii<=$stars; $iii++){
+        print('*');
+    };
+
+    print(PHP_EOL);
+
+    $stars++;
+    $pr--;
+};
+
+$stars = ($n*2)-3;
+$pr = 1;
+for($i=1; $i<=$n; $i++){
+
+    for($ii=1; $ii<=$pr; $ii++){
+        print(' ');
+    };
+
+    for($ii=1; $ii<=$stars; $ii++){
+        print('*');
+    };
+
+    print(PHP_EOL);
+
+    $stars = $stars-2;
+    $pr++;
+};
